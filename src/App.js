68a9cc84919d793bfcb926e4ac13fec96a7bc0f4
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2022-05-24 07:30:05 +0900

    finished logic for rolling dice

--- src/App.js
+++ src/App.js
@@ -6,14 +6,18 @@ import { nanoid } from "nanoid";
 function App() {
   const [allNewDice, setAllNewDice] = useState(newDice());
 
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    };
+  }
+
   function newDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push({
-        value: Math.ceil(Math.random() * 6),
-        isHeld: false,
-        id: nanoid(),
-      });
+      newDice.push(generateNewDie());
     }
     return newDice;
   }
@@ -42,11 +46,20 @@ function App() {
   }
 
   function handleClick() {
-    setAllNewDice(newDice());
+    setAllNewDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.isHeld ? die : generateNewDie();
+      })
+    );
   }
 
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
       <div className="dice-container">{mappedNewDice()}</div>
       <button className="roll-dice" onClick={handleClick}>
         Click
